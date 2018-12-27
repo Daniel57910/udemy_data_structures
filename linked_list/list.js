171cfd264830afe2ff3ce3003dcc5b735f2ed4e7
Daniel Miller <danielmiller5791@gmail.com>
2018-12-27 17:38:54 +0000

    list can be traversed via prev and next now

--- linked_list/list.js
+++ linked_list/list.js
@@ -3,20 +3,25 @@ const Node = require('./node')
 class LinkedList {
   constructor() {
     this.head = null
+    this.tail = null
     this.newNode
   }
 
   addToFront(element) {
     this.newNode = new Node(element)
-    !this.head ? this.createHead() : this.addElement()
+    !this.head ? this.createGenesisBlocks() : this.addElement()
   }
-  createHead() {
+
+  createGenesisBlocks() {
     this.head = this.newNode
+    this.tail = this.newNode
   }
   addElement() {
+    this.head.prev = this.newNode
     this.newNode.next = this.head
     this.head = this.newNode
   }
+
 }
 
 module.exports = LinkedList
--- linked_list/list.spec.js
+++ linked_list/list.spec.js
@@ -6,18 +6,29 @@ describe(`Single Linked List`, () => {
   })
   test(`instantiation of the linked list`, () => {
     expect(list.head).toEqual(null)
+    expect(list.tail).toEqual(null)
   })
   test(`adding one item to front of the list`, () => {
     list.addToFront(20)
     expect(list.head.data).toEqual(20)
+    expect(list.tail.data).toEqual(20)
+  })
+  test(`adding three items to create a head, middle and tail`, () => {
+    list.addToFront(30)
+    list.addToFront(49)
+    list.addToFront(56)
+    expect(list.head.data).toEqual(56)
+    expect(list.head.next.data).toEqual(49)
+    expect(list.tail.data).toEqual(30)
+    expect(list.tail.prev.data).toEqual(49)
   })
 
-  test(`adding several elements to the linked list`, () => {
-    list.addToFront(30)
-    list.addToFront(56)
-    list.addToFront(74)
-    expect(list.head.data).toEqual(74)
-    expect(list.head.next.data).toEqual(56)
-    expect(list.head.next.next.data).toEqual(30)
-  })
+  // test(`adding two items to the linked list`, () => {
+  //   list.addToFront(30)
+  //   list.addToFront(56)
+  //   list.addToFront(74)
+  //   expect(list.head.data).toEqual(74)
+  //   expect(list.head.next.data).toEqual(56)
+  //   expect(list.head.next.next.data).toEqual(30)
+  // })
 })
