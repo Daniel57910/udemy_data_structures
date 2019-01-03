09f17ad49033a3844222d9b743db06e52a1ebfd5
Daniel Miller <daniel.miller@openbet.com>
2019-01-03 16:33:40 +0000

    basic infrastructure for bsearch setup

---
+++ binary_tree/lib/btree.js
@@ -0,0 +1,6 @@
+class BinaryTree {
+
+}
+
+
+module.exports = BinaryTree
---
+++ binary_tree/spec/btree.spec.js
@@ -0,0 +1,10 @@
+const BinaryTree = require('../lib/btree')
+
+describe('Basic Binary Search Tree', () => {
+	beforeEach(() => {
+		binaryTree = new BinaryTree()
+	})
+	test('instantiation of the basic binary search tree', () => {
+		expect(true).toEqual(true)
+	})
+})
