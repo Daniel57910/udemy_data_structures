adc58a9bcbc04667057cf5e955088a0a405a4c8d
Daniel Miller <daniel.miller@openbet.com>
2019-01-03 16:56:47 +0000

    basic insertion passing

--- binary_tree/lib/btree.js
+++ binary_tree/lib/btree.js
@@ -1,4 +1,33 @@
 class BinaryTree {
+	constructor(value) {
+		this.value = value
+		this.right = this.left = null
+	}
+
+	insert(data) {
+		if (data <= this.value) {
+			if (!this.left) {
+				this.left = new BinaryTree(data)
+				return
+			}
+			else {
+				this.value = this.left.value
+				this.insert(value)
+			}
+		}
+		else {
+			if (!this.right) {
+				this.right = new BinaryTree(data)
+				return
+			}
+			else {
+				this.right = this.right.value
+				this.insert(value)
+			}
+		}
+	}
+			
+	
 
 }
 
--- binary_tree/spec/btree.spec.js
+++ binary_tree/spec/btree.spec.js
@@ -2,9 +2,17 @@ const BinaryTree = require('../lib/btree')
 
 describe('Basic Binary Search Tree', () => {
 	beforeEach(() => {
-		binaryTree = new BinaryTree()
+		binaryTree = new BinaryTree(50)
 	})
 	test('instantiation of the basic binary search tree', () => {
-		expect(true).toEqual(true)
+		expect(binaryTree.value).toEqual(50)
+		expect(binaryTree.left).toEqual(null)
+		expect(binaryTree.right).toEqual(null)
+	})
+	test('adding one item to the left', () => {
+		binaryTree.insert(30)
+		expect(binaryTree.left.value).toEqual(30)
+		expect(binaryTree.left.left).toEqual(null)
+		expect(binaryTree.left.right).toEqual(null)
 	})
 })
